6e9f061257cd349a82b0c2b0b54b7157deb903d1
gpdzvapatsva <gpdzvapatsva@gmail.com>
2024-10-08 17:25:17 +0100

    dev branch changes

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-   # path('about/', views.about, name='about'),
-    path('about/', views.about, name='about-page'),
-    # path('about/', views.about, name='about'),
-    path('skills/', views.skills, name='skills-page'),
-    # path('about/', views.about, name='about'),
+
+    path('about/', views.about, name='about'),
+
+    path('skills/', views.skills, name='skills'),
+path('contact/', views.contact, name='contact'),
+
 ]
